fdf8c99b5fdc591125db3d2d599cd90079aaa859
Yuvi Masory <ymasory@gmail.com>
2010-07-20 12:45:58 -0400

    first commit

---
+++ src/MyProgram.scala
@@ -0,0 +1,12 @@
+object MyProgram {
+
+  /** This is a sample Scala program
+    * 
+    * You can add whatever you want, but if you change the name
+    * of the entry point object (MyProgram) you need to update
+    * the build.xml file to reflect that change.
+    * @param args - Not used */
+  def main(args: Array[String]) {
+    println("welcome to MyProgram")
+  }
+}
---
+++ test/MyProgramTest.scala
@@ -0,0 +1,8 @@
+import org.scalatest.FunSuite
+
+class MyProgramTest extends FunSuite {
+  
+  test("dummy") {
+    assert(1 === 1)
+  }
+}
